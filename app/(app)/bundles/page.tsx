6ae3824cd96ac538ef959264d0ef26347939de5b
DimensionCoin <nick.vassallo97@gmail.com>
2026-01-20 22:31:03 -0500

    tiny wording fix

--- app/(app)/bundles/page.tsx
+++ app/(app)/bundles/page.tsx
@@ -53,7 +53,7 @@ export default function BundlesPage() {
               `}
             >
               <Sparkles className="h-4 w-4" />
-              <span className="hidden sm:inline">User</span> Bundles
+              Community
             </button>
           </div>
 
